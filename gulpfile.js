c3c62ff09aed79b23377adaad7ac3d7e5d1b9303
Stanislav Byneev <byneev.frontend@gmail.com>
2022-03-23 21:21:58 +0300

    fix: correct layout issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var purge = require('gulp-css-purge');
 
 gulp.task('sass', function () {
   return gulp.src('./src/scss/app.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(purge())
     .pipe(gulp.dest('./src/css/'));
 });
 
