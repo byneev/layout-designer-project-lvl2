93d5847a554d93e5aeb5bbfc41e8ee77dd150813
Stanislav Byneev <byneev.frontend@gmail.com>
2022-04-01 15:31:42 +0300

    feat: correct gulpfile, correct issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass')(require('sass'));
 var purge = require('gulp-css-purge');
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/app.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src("src/scss/app.scss")
+    .pipe(sass())
     .pipe(purge())
-    .pipe(gulp.dest('./src/css/'));
+    .pipe(gulp.dest("src/css/"))
+    .pipe(browserSync.stream());
+});
+// Static Server + watching scss/html files
+gulp.task('serve', function () {
+
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch("src/scss/**/*.scss", gulp.series('sass'));
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/**/*.scss', gulp.series('sass'));
-});
+gulp.task('default', gulp.series('serve'));
